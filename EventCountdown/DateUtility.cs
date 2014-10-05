6527ce86fa362ff9d5507a3cfc8d846e09d89d28
RichTeaMan <thomasholmes@outlook.com>
2014-10-05 20:36:01 +0100

    Added countdown for Christmas.

---
+++ EventCountdown/DateUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCountdown
+{
+    public class DateUtility
+    {
+        public static TimeSpan GetXmasTimespan
+        {
+            get
+            {
+                var now = DateTime.Now;
+                var xmas = new DateTime(now.Year, 12, 25);
+                return xmas - now;
+            }
+        }
+
+        public static int GetXmasDays
+        {
+            get
+            {
+                var daysF = GetXmasTimespan.TotalDays;
+                var days = (int)Math.Ceiling(daysF);
+                return days;
+            }
+        }
+    }
+}
